02f39a107e801de99b9839dd8144430a7beab4a6
SHENO1981 <139652899+SHENO1981@users.noreply.github.com>
2026-09-11 09:22:49 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,4 @@
-const C='modyar-v3-20260911';
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json'])));
-});
-self.addEventListener('activate',e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))
-  .then(()=>self.clients.claim())
-));
-self.addEventListener('fetch',e=>e.respondWith(
-  fetch(e.request).catch(()=>caches.match(e.request))
-));
+const C='modyar-v4-20260911';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json'])))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
