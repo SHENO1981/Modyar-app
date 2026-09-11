225be85c62710e47c1d56b74774e74828e47bc17
shno1981 <139652899+SHENO1981@users.noreply.github.com>
2026-09-11 20:59:15 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='modyar-v7-20260911';
+const C='modyar-v8-20260911';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json'])))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
