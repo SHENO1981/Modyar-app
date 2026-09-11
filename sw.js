cf566051596aeb0244ae579eddeb02f61916261f
SHENO1981 <139652899+SHENO1981@users.noreply.github.com>
2026-09-11 08:55:01 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-const C='honarestan-mobile-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='modyar-v3-20260911';
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json'])));
+});
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))
+  .then(()=>self.clients.claim())
+));
+self.addEventListener('fetch',e=>e.respondWith(
+  fetch(e.request).catch(()=>caches.match(e.request))
+));
